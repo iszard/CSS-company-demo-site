0717ba80dbfeb6baaae358f9e0651cdc7278828f
Gavyn Elrick <gavynelrick@gmail.com>
2025-01-01 14:48:26 -0500

    First source code commit: - Home page -- Header banner with mobile hamburger menu and desktop inline menu -- Hero banner -- Plan choice UI with mobile and desktop styling. -- Good reasons UI with mobile and desktop styling. -- Footer with tags - Packages page -- Details about packages with mobile and desktop styling. - Customers page -- Customer feed back details and images with mobile and desktop styling. - Start hosting page [This page is still under heavy development] -- Forum elements for user input with mobile and desktop styling.

---
+++ shared.js
@@ -0,0 +1,37 @@
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var modalNoButton = document.querySelector(".modal__action--negative");
+var selectPlanButton = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+
+for (var i = 0; i < selectPlanButton.length; i++) {
+  selectPlanButton[i].addEventListener("click", () => {
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    modal.classList.add("open");
+    backdrop.classList.add("open");
+  });
+}
+
+var closeModal = () => {
+  //   modal.style.display = "none";
+  //   backdrop.style.display = "none";
+  modal?.classList.remove("open");
+  backdrop.classList.remove("open");
+};
+
+backdrop.addEventListener("click", () => {
+  //   mobileNav.style.display = "none";
+  mobileNav.classList.remove("open");
+  closeModal();
+});
+
+modalNoButton?.addEventListener("click", closeModal);
+
+toggleButton.addEventListener("click", () => {
+  //   mobileNav.style.display = "block";
+  //   backdrop.style.display = "block";
+  mobileNav.classList.add("open");
+  backdrop.classList.add("open");
+});
